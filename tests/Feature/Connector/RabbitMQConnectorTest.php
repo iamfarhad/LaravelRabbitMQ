2f64f205346b093000349dee06ff33ca311757e0
iamfarhad <farhad.pd@gmail.com>
2023-01-19 00:39:47 +0330

    update tests

--- tests/Feature/Connector/RabbitMQConnectorTest.php
+++ tests/Feature/Connector/RabbitMQConnectorTest.php
@@ -18,12 +18,12 @@ class RabbitMQConnectorTest extends FeatureTestCase
     {
         $getQueueInstance = $this->app['queue'];
 
-        $this->app['config']->set('queue.connections.rabbitmq.hosts.lazy', true);
+//        $this->app['config']->set('queue.connections.rabbitmq.hosts.lazy', true);
 
         $connection = $getQueueInstance->connection('rabbitmq');
 
         $this->assertInstanceOf(RabbitQueue::class, $connection);
-        $this->assertInstanceOf(AMQPLazyConnection::class, $connection->getConnection());
+        $this->assertInstanceOf(AMQPStreamConnection::class, $connection->getConnection());
         $this->assertTrue($connection->getConnection()->isConnected());
         $this->assertTrue($connection->getConnection()->channel()->is_open());
     }
